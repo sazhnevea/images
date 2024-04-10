67514b1e35a989fd1bbbadcb6d6fb7cc10395625
Evgenii Sazhnev <sazhnevea@gmail.com>
2024-04-09 20:46:18 -0500

    works

--- impose/resize.js
+++ impose/resize.js
@@ -74,6 +74,14 @@ export const resizePhoto = async (photo, sizeType, layoutWidth, layoutHeight, or
       resizedPhoto = await photo.resize(updatedWidth, updatedHeight).sharpen({ sigma: 1 })
       break
     }
+    case SIZE_TYPES.TWO_VERTICAL_RIGHT_CENTER: {
+      if (order === 1 || order === 2) {
+        updatedWidth = Math.round((layoutWidth.getHalf().minusMargin() - doubleXPadding - innerPadding) / 2 );
+        updatedHeight = Math.round((layoutHeight.minusMargins() - (doubleYPadding) - innerPadding) / 2);
+        resizedPhoto = await photo.resize(updatedWidth, updatedHeight).sharpen({ sigma: 1 })
+      }
+      break
+    }
     case SIZE_TYPES.COVER: {
       updatedWidth = 1359;
       updatedHeight = 2040;
