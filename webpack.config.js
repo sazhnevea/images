4e04b293e108cf4c1d1860db180f7ac07774e111
Evgenii Sazhnev <esazhnev@rutube.com>
2024-11-19 21:48:24 +0400

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,32 @@
 import { fileURLToPath } from 'url';
-import { resolve } from 'path';
-import { dirname } from 'path';
+import { resolve, dirname } from 'path';
+import { existsSync, cpSync } from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // eslint-disable-next-line no-undef
-const folderName = process.env.FOLDER_NAME
+const bundleName = process.env.BUNDLE_NAME;
+
+const copyAssets = () => {
+  const source = resolve(__dirname, 'assets');
+  const destination = resolve(__dirname, 'build', 'assets');
+
+  if (existsSync(source)) {
+    cpSync(source, destination, { recursive: true });
+    console.log('Assets folder copied successfully!');
+  } else {
+    console.warn('Assets folder does not exist, skipping...');
+  }
+};
+
+copyAssets();
 
 export default {
-  entry: `./${folderName}/index.js`,
+  entry: `./${bundleName}/index.js`,
   output: {
-    filename: 'bundle.cjs',
-    path: resolve(__dirname, 'dist', folderName),
+    filename: `${bundleName}.cjs`,
+    path: resolve(__dirname, 'build'),
     libraryTarget: 'commonjs2',
   },
   mode: 'production',
